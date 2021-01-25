a4e3e293f9f06f32136fe719dc044fbebb35f41c
Simon Aronsson <simme@arcticbit.se>
2021-01-25 18:25:55 +0100

    include js files in dist

--- chaosk6/__init__.py
+++ chaosk6/__init__.py
@@ -12,7 +12,7 @@ from chaoslib.types import Discovery, DiscoveredActivities, Secrets
 """Top-level package for chaostoolkit-k6."""
 
 __all__ = ["discover"]
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 
 
 def discover(discover_system: bool = True) -> Discovery:
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with io.open("requirements.txt") as f:
 
 setup_params = dict(
     name=name,
-    version="0.1.2",
+    version="0.1.4",
     description=desc,
     long_description=long_desc,
     classifiers=classifiers,
