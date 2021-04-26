101b58e0992e3adbbb8c7156e43d8677028c6971
Simon Aronsson <simme@arcticbit.se>
2021-04-26 17:49:16 +0200

    bump version

--- chaosk6/__init__.py
+++ chaosk6/__init__.py
@@ -12,7 +12,7 @@ from chaoslib.types import Discovery, DiscoveredActivities
 """Top-level package for chaostoolkit-k6."""
 
 __all__ = ["discover"]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 def discover(discover_system: bool = True) -> Discovery:
