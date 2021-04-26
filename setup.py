458b5642a802fa84bc5172397f1bdaad7afc223a
Simon Aronsson <simme@arcticbit.se>
2021-04-26 17:48:31 +0200

    bump version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with io.open("requirements.txt") as f:
 
 setup_params = dict(
     name=name,
-    version="0.2.0",
+    version="0.3.0",
     description=desc,
     long_description=long_desc,
     classifiers=classifiers,
